d5022a3df1c124356b799a34365b7559f43f6070
YanVI9 <yan.itskevich@gmail.com>
2018-12-03 22:08:02 +0300

    Футер

--- vitaam_wp/footer.php
+++ vitaam_wp/footer.php
@@ -1,5 +1,35 @@
 </div>
-	<footer></footer>
+<footer class="footer">
+        <div class="footer__block">
+            <div class="footer__menu">
+                <div class="menu__block">
+                    <a href="#">Vitaam</a>
+                    <a href="#">Протеиновые смеси</a>
+                    <a href="#">Где купить</a>
+                    <a href="#">Рецепты</a>
+                </div>
+                <div class="menu__block">
+                    <a href="#">Новым клиентам</a>
+                    <a href="#">Дилерам</a>
+                    <a href="#">Блог</a>
+                    <a href="#">Контакты</a>
+                </div>
+            </div>
+            <div class="footer__centerBlock">
+                <div class="footer__socialLinks">
+                    <a href="#" class="footer__vk"><img src="img/vk_footer.png" alt=""></a>
+                    <a href="#" class="footer__fb"><img src="img/fb_footer.png" alt=""></a>
+                    <a href="#" class="footer__insta"><img src="img/insta_footer.png" alt=""></a>
+                </div>
+                <a href="#" class="footer__logo"><img src="img/logo_footer.png" alt=""></a>
+            </div>
+            <div class="footer__contacts">
+                <p><a href="tel:88002002525">8 800 200 25 25</a></p>
+                <p><a href="tel:+74951282785">+7 495 128 27 85</a></p>
+                <p><a href="mail:hello@vitaam.ru">hello@vitaam.ru</a></p>
+            </div>
+        </div>
+    </footer>
 	<?php wp_footer(); ?>
 </body>
 </html>
