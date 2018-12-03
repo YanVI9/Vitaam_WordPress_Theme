786fe92f0b42b3a5f27df3aee83bac950a69b824
YanVI9 <yan.itskevich@gmail.com>
2018-12-03 22:33:43 +0300

    Добавлен файл главной страницы

---
+++ vitaam_wp/front-page.php
@@ -0,0 +1,265 @@
+<?php get_header(); ?>
+<section class="banner">
+        <div class="banner__text-block">
+            <p>Правильное питание с Vitaam</p>
+            <p>Вкусная ВЫПЕЧКА БЕЗ сахара, глютена и расширения талии, Vitaam – протеиновые смеси для низкокалорийной выпечки</p>
+            <p>Максимум белка и пользы для фигуры в каждом тортике</p>  
+        </div>  
+    </section>
+    <section class="about">
+        <div class="about__text-block">
+            <p><video src=""></video></p>
+            <p class="break-after">Купить сухую смесь для выпечки можно в онлайн-магазине Vitaam, точках продажи и в нтернет-магазинах. Официальных дилеров нашей компании ищите в разделе <a href="#">«Где купить»</a>. Цена протеиновой смеси одинаковая, но у дилера чаще дешевле и быстрее доставка.</p>
+            <p>Компания ООО «Витаам» производит <span>диетические низкокалорийные сухие смеси для выпечки</span> протеиновые смеси) из кокосовой муки, муки чиа и киноа, протеина, безопасного заменителя cахара Fibrulose E97, пробиотиков и пищевых волокон.</p>
+            <p>В наших протеиновых смесях нет сахара, глютена, вредных для здоровья химических и биологических веществ. <span>Минимальное содержание углеводов</span> позволяет потреблять выпекаемый продукт людям, которые следят за своим здоровьем и фигурой. </p>
+            <p>Протеиновая смесь для выпечки Vitaam <span>легко растворяется в воде или молоке</span>, не сжимается в комки и не прилипает к стенкам посуды. Её нужно залить в пропорции 1 к 2, взбить миксером 30 секунд или ложкой в течение 5 минут. Вот и всё. <span>Можно готовить низкокалорийную, полезную и высокобелковую выпечку</span> по нашим рецептам, из интернета или своей книги рецептов. Наслаждайтесь «запретными» блюдами со смесями для выпечки Vitaam.</p>
+        </div>
+    </section>
+    <section class="privilege">
+        <div class="privilege__block">
+            <div class="privilege__item">
+                <div class="privilege__iconBlock privilege__icon-micro"></div>
+                <div class="privilege__textBlock">
+                    <h3 class="privilege__h3">Разработано совместно с экспертами</h3>
+                    <p class="privilege__p">Смесь разработана совместно с НИИ питания РАМН (Россия) и консультантами-диетологами из США. Продукция сертифицирована.</p>
+                </div>
+            </div>
+            
+            <div class="privilege__item">
+                <div class="privilege__iconBlock privilege__icon-box"></div>
+                <div class="privilege__textBlock">
+                    <h3 class="privilege__h3">Удобная упаковка для хозяек и розничной продажи</h3>
+                    <p class="privilege__p">Прямоугольная коробка со 100 граммами смеси. Удобно хранить, готовить без остатков, легко подбирать нужное количество жидкости.</p>
+                </div>
+            </div>
+
+            <div class="privilege__item">
+                <div class="privilege__iconBlock privilege__icon-tree"></div>
+                <div class="privilege__textBlock">
+                    <h3 class="privilege__h3">Безопасные компоненты со всего мира</h3>
+                    <p class="privilege__p">В составе более 100 экологичных компонентов из Франции, Чили, Уругвая, Перу, Индии, США, и России. Каждая смесь индивидуальна.</p>
+                </div>
+            </div>
+
+            <div class="privilege__item">
+                <div class="privilege__iconBlock privilege__icon-apple"></div>
+                <div class="privilege__textBlock">
+                    <h3 class="privilege__h3">Новый продукт для создания низкокалорийной выпечки</h3>
+                    <p class="privilege__p">Минимум калорий, максимум белка, пищевых волокон и пробиотиков для приготовления здоровой выпечки.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="sale">
+        <div class="sale__block">
+            <h2 class="sale__caption">Своим скидки на смеси для выпечки Vitaam</h2>
+            <div class="sale__box">
+                <div class="sale__item">
+                    <h3 class="sale__h3">Скидка 10%</h3>
+                    <p class="sale__p">участнику группы ВК или Инстаграм и при покупке</p>
+                    <p class="sale__p-sale">от 2000 р.</p>
+                </div>
+                <div class="sale__item">
+                    <h3 class="sale__h3">Бесплатная доставка</h3>
+                    <p class="sale__p">по России участнику группы ВК или Инстаграм при покупке</p>
+                    <p class="sale__p-sale">от 3000 р.</p>
+                </div>
+                <div class="sale__item">
+                    <h3 class="sale__h3">В2В</h3>
+                    <p class="sale__p">оплатив предзаказ продукции, подробности у менеджеров</p>
+                    <p class="sale__p-sale">Сэкономьте 15%</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="products">
+        <h2>Продуктовая линейка</h2>
+        <p class="products__text">Перед вами <span>мир низкокалорийных тортов, печенья, блинов и пиццы</span> с заботой о своей фигуре. Это просто сказка какая-то для людей, желающих есть здоровую пищу, не изменяя свои вкусовые предпочтения. Каждая смесь для выпечки индивидуальна по составу и подходит для приготовления определенного набора блюд.</p>
+        <div class="products__container">
+            <div class="products__item"><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/blinchik.png" alt="Блинчики"></a></div>
+            <div class="products__item"><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/biskvit.png" alt="Бисквит"></a></div>
+            <div class="products__item"><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/maffin.png" alt="Маффины"></a></div>
+            <div class="products__item"><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/pizza.png" alt="Пицца"></a></div>
+            <div class="products__item"><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/pechenie.png" alt="Печенье"></a></div>
+            <div class="products__item"><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/pudding.png" alt="Пуддинг"></a></div>
+        </div>
+    </section>
+    <section class="partners">
+        <h2>Партнеры и отзывы о сухих протеиновых смесях для выпечки Vitaam </h2>
+        <div class="partners__slider">
+            <label for="#"></label>
+            <label for="#"></label>
+            <div class="slider__container">
+                <div class="slider__item"></div>
+                <div class="slider__item"></div>
+                <div class="slider__item"></div>
+                <div class="slider__item"></div>
+                <div class="slider__item"></div>
+            </div>
+        </div>
+    </section>
+    <section class="feedback">
+        <div class="feedback__container">
+            <div class="feedback__item">
+                <div class="feedback__line1">
+                    <div class="feedback__text">
+                        «Регулярно покупаем смеси Vitaam для приготовления вкусных тортов и блинов. Гости Кремля довольны вкусом и весьма удивлены низкой калорийностью»...
+                    </div>
+                    <div class="feedback__avatara">
+                        <img src="<?php bloginfo('template_url'); ?>/images/original.png" alt="Ава">
+                    </div>
+                </div>
+                <div class="feedback__line2">
+                    <p>
+                        Путин Владимир Владимирович, <br>СЕО компании «Россия»
+                    </p>
+                </div>
+            </div>
+            <div class="feedback__item">
+                <div class="feedback__line1">
+                    <div class="feedback__text">
+                        «Регулярно покупаем смеси Vitaam для приготовления вкусных тортов и блинов. Гости Кремля довольны вкусом и весьма удивлены низкой калорийностью»...
+                    </div>
+                    <div class="feedback__avatara">
+                        <img src="<?php bloginfo('template_url'); ?>/images/original.png" alt="Ава">
+                    </div>
+                </div>
+                <div class="feedback__line2">
+                    <p>
+                        Путин Владимир Владимирович, <br>СЕО компании «Россия»
+                    </p>
+                </div>
+            </div>
+            <div class="feedback__item">
+                <div class="feedback__line1">
+                    <div class="feedback__text">
+                        «Регулярно покупаем смеси Vitaam для приготовления вкусных тортов и блинов. Гости Кремля довольны вкусом и весьма удивлены низкой калорийностью»...
+                    </div>
+                    <div class="feedback__avatara">
+                        <img src="<?php bloginfo('template_url'); ?>/images/original.png" alt="Ава">
+                    </div>
+                </div>
+                <div class="feedback__line2">
+                    <p>
+                        Путин Владимир Владимирович, <br>СЕО компании «Россия»
+                    </p>
+                </div>
+            </div>
+            <div class="feedback__item">
+                <div class="feedback__line1">
+                    <div class="feedback__text">
+                        «Регулярно покупаем смеси Vitaam для приготовления вкусных тортов и блинов. Гости Кремля довольны вкусом и весьма удивлены низкой калорийностью»...
+                    </div>
+                    <div class="feedback__avatara">
+                        <img src="<?php bloginfo('template_url'); ?>/images/original.png" alt="Ава">
+                    </div>
+                </div>
+                <div class="feedback__line2">
+                    <p>
+                        Путин Владимир Владимирович, <br>СЕО компании «Россия»
+                    </p>
+                </div>
+            </div>    
+        </div>
+        <a href="#" class="feedback__more">Отзывы полностью</a>
+    </section>
+    <section class="recepts">
+        <h2>Популярные рецепты от Vitaam</h2>
+        <div class="recepts_box">
+            <div class="recepts_item">
+                <a href="#" class="recepts__link">
+                    <img src="<?php bloginfo('template_url'); ?>/images/maffins_recept.png" alt="" class="recepts__image">
+                    <div class="recepts_filling">
+                        <p class="recepts__text">Вкусные маффины без вреда для фигуры</p>
+                    </div>
+                </a>
+            </div>
+            <div class="recepts_item">
+                <a href="#" class="recepts__link recepts__link_pudding">
+                    <img src="<?php bloginfo('template_url'); ?>/images/pudding_recept.png" alt="" class="recepts__image">
+                    <div class="recepts_filling">
+                        <p class="recepts__text">Вкусный пуддинг без вреда для фигуры</p>
+                    </div>
+                </a>
+            </div>
+            <div class="recepts_item">
+                <a href="#" class="recepts__link">
+                    <img src="<?php bloginfo('template_url'); ?>/images/blins_recept.png" alt="" class="recepts__image">
+                    <div class="recepts_filling">
+                        <p class="recepts__text">Вкусные блины без вреда для фигуры</p>
+                    </div>
+                </a>
+            </div>
+            <div class="recepts_item">
+                <a href="#" class="recepts__link">
+                    <img src="<?php bloginfo('template_url'); ?>/images/puzza_recept.png" alt="" class="recepts__image">
+                    <div class="recepts_filling">
+                        <p class="recepts__text">Вкусная пицца без вреда для фигуры</p>
+                    </div>
+                </a>
+            </div>
+            <div class="recepts_item">
+                <a href="#" class="recepts__link">
+                    <img src="<?php bloginfo('template_url'); ?>/images/pech_recept.png" alt="" class="recepts__image">
+                    <div class="recepts_filling">
+                        <p class="recepts__text">Вкусное печенье без вреда для фигуры</p>
+                    </div>
+                </a>
+            </div>
+            <div class="recepts_item">
+                <a href="#" class="recepts__link">
+                    <img src="<?php bloginfo('template_url'); ?>/images/blins_recept-1.png" alt="" class="recepts__image">
+                    <div class="recepts_filling">
+                        <p class="recepts__text">Вкусные блины без вреда для фигуры</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </section>
+    <section class="contacts">
+        <div class="social__linkBox contacts__item">
+            <div class="linkBox__block linkBox__block_vk">
+                <img src="<?php bloginfo('template_url'); ?>/images/vk__logo.png" alt="VK" class="linkBox__img">
+                <a href="#" class="linkBox__link">Подписаться</a>
+            </div>
+            <div class="linkBox__block linkBox__block_insta">
+                <img src="<?php bloginfo('template_url'); ?>/images/instagram__logo.png" alt="INSTAGRAM" class="linkBox__img">
+                <a href="#" class="linkBox__link">Подписаться</a>
+            </div>
+        </div>
+        <div class="social__textBlock contacts__item">
+            <p class="textBlock__caption">Подписывайтесь на Инстаграм и группу ВКонтакте и покупайте со скидкой 10%</p>
+            <ul class="list">
+                <li class="list__item">Изучайте новые рецепты;</li>
+                <li class="list__item">Готовьте и экспериментируйте;</li>
+                <li class="list__item">Общайтесь;</li>
+                <li class="list__item">Присылайте свои рецепты со смесями для выпечки Vitaam и принимайте ценные подарки.</li>
+            </ul>
+        </div>
+        <form action="#" class="contacts__form contacts__item">
+            <input type="text" placeholder="Имя" name="name" class="form__name"><input type="text" placeholder="Телефон" name="phone" class="form__phone">
+            <textarea name="message" id="message" placeholder="Что вы хотите?"></textarea>
+            <div class="form__footer">
+                <div class="form__privacyPolicyBox">
+                    <input type="checkbox" name="privacyPolicy" id="privacyPolicy">
+                    <label for="privacyPolicy"></label>
+                    <a href="#">Политика конфиденциальности</a>
+                </div>
+                <button>Отправить</button>
+            </div>
+        </form>
+        <div class="contacts__phoneBox contacts__item">
+            <p class="phoneBox__number">8 800 200 25 25</p>
+            <p class="phoneBox__number">+7 495 128 27 85</p>
+            <p class="phoneBox__text">Звоните, оставляйте заявку через форму обратной связи или корзину. Менеджер свяжется с вами для уточнения заказа, ответит на вопросы и поможет провести сделку.  </p>
+        </div>
+    </section>
+
+
+
+<!--<section>
+	<?php /*if (have_posts()): while (have_posts()): the_post(); ?>
+		<?php the_content(); ?>
+	<?php endwhile; endif;*/ ?>
+</section>-->
+<?php get_footer(); ?>
